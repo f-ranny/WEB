1c0556c110f5a89fe89037c41be97ec381ecf8ad
Bankue Franklin <69907754+f-ranny@users.noreply.github.com>
2022-09-27 16:46:48 +0300

    Add files via upload

---
+++ chess-react/src/App.tsx
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import BoardComponents from "./components/BoardComponents";
+import {Board} from "./models/Board";
+import {Colors} from "./models/Colors";
+import {Player} from "./models/Player";
+import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
+
+function App() {
+    const [board, setBoard] = useState(new Board())
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
+
+    useEffect(() => {
+        restart();
+    }, [])
+
+    function restart() {
+        const newBoard = new Board();
+        newBoard.initCells()
+        newBoard.addFigures()
+        setBoard(newBoard)
+        setCurrentPlayer(whitePlayer);
+    }
+
+    function swapPlayer() {
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
+    }
+
+  return (
+    <div className="app">
+        <Timer
+            currentPlayer={currentPlayer}
+            restart={restart}
+            />
+      <BoardComponents
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+        <div>
+            <LostFigures
+                title="Черные фигуры"
+                figures={board.lostBlackFigures}
+                />
+            <LostFigures
+                title="Белые фигуры"
+                figures={board.lostWhiteFigures}
+                />
+        </div>
+    </div>
+  );
+}
+
+export default App;
